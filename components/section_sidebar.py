d300eb73bf8da95a3f20fdc974f9c3b85e22cf21
Lan-Anh-2910 <153609816+Lan-Anh-2910@users.noreply.github.com>
2026-02-10 14:33:40 +0700

    Update section_sidebar.py

--- components/section_sidebar.py
+++ components/section_sidebar.py
@@ -4,16 +4,7 @@ def render_section_sidebar(sections):
     st.markdown("### Sections")
 
     for sec in sections:
-        # Parent
         st.markdown(
             f"- [{sec['label']}](#{sec['anchor']})",
             unsafe_allow_html=True
         )
-
-        # Children
-        if "children" in sec:
-            for child in sec["children"]:
-                st.markdown(
-                    f"  - [{child['label']}](#{child['anchor']})",
-                    unsafe_allow_html=True
-                )
